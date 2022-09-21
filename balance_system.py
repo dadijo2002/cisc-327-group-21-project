487037b7e8ea2fae6b9d2a2dd56477f44cb8ebaf
unknown <aniketm7274@gmail.com>
2022-09-21 17:48:37 -0400

    added more documentation and an init function.

--- balance_system.py
+++ balance_system.py
@@ -2,18 +2,27 @@ user_db = {"name": "Sam Fisher",
            "balance": 5643,
            "deposit method": "credit card",
            "expiry date": "04/27"}  # this is meant to simulate an actual database with data entries for every user
+
+
 # more data entries would be necessary for all the things we're going to want to do with this class/entity/model
+# will likely use an actual off-site sql database to store user balance data and pull stuff from there instead.
 
 
-class Balance():
+class Balance:
     # properties include actual total, deposit method, validity of deposit method
     total = user_db["balance"]  # this would be fetching the data from an external database normally
-    deposit_method = user_db["deposit method"] # as would this
+    deposit_method = user_db["deposit method"]  # as would this
     validity = False
     # to check for validity, we'd need the length of the card number,
     # read the expiry date and the 3 digit code at the back, and that's just for using credit cards
-    if user_db["deposit method"] == "credit card":
+    if user_db["deposit method"] == "credit card":  # checking to see if the user chose to deposit via credit cards
         if len(user_db["deposit method"]) != 16:
+            # just checking to see if the credit card number is long enough for now
             validity = False
         else:
             validity = True
+
+    def __init__(self):  # initialization function
+        total = self.total
+        deposit_method = self.deposit_method
+        validity = self.validity
