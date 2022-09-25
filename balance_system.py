a9d4b6a81696f3d48579703df9e721df09783997
unknown <aniketm7274@gmail.com>
2022-09-25 12:50:17 -0400

    changed formatting of comments to better adhere to the PEP 8 standard.

--- balance_system.py
+++ balance_system.py
@@ -10,16 +10,18 @@ Date: September 23, 2022
 user_db = {"name": "Sam Fisher",
            "balance": 5643,
            "deposit method": "credit card",
-           "expiry date": "04/27"}  # this is meant to simulate an actual database with data entries for every user
+           "expiry date": "04/27"}
+# this is meant to simulate an actual database with data entries for every user
 
 
-# more data entries would be necessary for all the things we're going to want to do with this class/entity/model
-# will likely use an actual off-site sql database to store user balance data and pull stuff from there instead.
+# more data entries would be necessary
+# will likely use an off-site sql database to store user balance data
 
 
 class Balance:
     # properties include actual total, deposit method, validity of deposit method
-    total = user_db["balance"]  # this would be fetching the data from an external database normally
+    total = user_db["balance"]
+    # this would be fetching the data from an external database normally
     deposit_method = user_db["deposit method"]  # as would this
     validity = False  # assume it's false until we run the validate function
 
@@ -35,12 +37,12 @@ class Balance:
         """
 
         # to check for validity, we'd need the length of the card number,
-        # read the expiry date and the 3 digit code at the back, and that's just for using credit cards
+        # read the expiry date and the 3 digit code at the back
+        # and that's just for using credit cards
         if user_db["deposit method"] == "credit card":  # checking to see if the user chose to deposit via credit cards
             if len(user_db["deposit method"]) != 16 and user_db["expiry date"] != "04/27":
-                # checking if the credit card number is long enough to be a valid credit card
-                # also need to check if the expiry date is of valid format, was planning on doing that, just couldn't
-                # quite remember how just yet
+                # checking if the credit card number has enough digits
+                # also need to check if the expiry date is of valid format
                 return False
             else:
                 return True
