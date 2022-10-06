199e194553bf7ce1df5238c4e3cd47fd8b278a2c
Daniel <48493942+dadijo2002@users.noreply.github.com>
2022-10-05 23:44:54 -0400

    💚 Implemented R2 and R3
    Description
    Implemented all requirements regarding R2 & R3
    
    Author: # Daniel Dickson
    Fixes # added some requirements
    
    Type of change
    Please delete options that are not relevant.
    
    New feature (non-breaking change which adds functionality)
    
    I added documentation to accompany these changes
    
    Checklist:
    [x ] My code follows the style guidelines of this project (PEP 8)
    [ x] I have performed a self-review of my own code
    [ x] I have commented my code, particularly in hard-to-understand areas
    [ x] My changes generate no new warnings
    [ x] Any dependent changes have been merged and published in downstream modules

--- user_system.py
+++ user_system.py
@@ -1,9 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-
-from flask_sqlalchemy import SQLAlchemy
+from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -32,6 +30,196 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+    def login(self):
+        """
+        This function validates username and password before login is completed.
+        """
+        username = input("Username: ")
+        password = input("Password: ")
+        # TODO: find some way to obscure passwords at login?
 
+        caps_check = 0
+        lower_check = 0
+        num_check = 0
 
+        # verify username format
+        if len(username) != 0 and username.isalnum():
 
+            for char in password:
+                if char.isupper():
+                    caps_check += 1
+                elif char.islower():
+                    lower_check += 1
+                elif char.isnumeric():
+                    num_check += 1
+
+            # verify password format
+            if (len(password) >= 6 and password.isalnum() and caps_check >= 1 and
+                lower_check >= 1 and num_check >= 1):
+
+                # if both user & pass are valid, check database for existing user
+                result = User.query.filter_by(username=username).first()
+
+                # if user is found, check that password matches for that user
+                if result != None:
+
+                    if result.password == password:
+                        print("Login successful! Welcome back, " + username + "!")
+
+                    else:
+                        print("Incorrect password! Please try again.")
+
+                else:
+                    print("Username not found!")
+
+            else:
+                print("Invalid password format!")
+
+        elif (" " in username and username[0] != " " and
+            username[-1] != " "):
+            temp_user = username.replace(" ", "")
+
+            # If no special characters other than space and format allowed
+            # Check that no existing account already has this username
+            if temp_user.isalnum():
+
+                for char in password:
+                    if char.isupper():
+                        caps_check += 1
+                    elif char.islower():
+                        lower_check += 1
+                    elif char.isnumeric():
+                        num_check += 1
+
+                # verify password format
+                if (len(password) >= 6 and password.isalnum() and caps_check >= 1 and
+                    lower_check >= 1 and num_check >= 1):
+
+                    # if both user & pass are valid, check database for existing user
+                    result = User.query.filter_by(username=username).first()
+
+                    # if user is found, check that password matches for that user
+                    if result != None:
+
+                        if result.password == password:
+                            print("Login successful! Welcome back, " + username + "!")
+
+                        else:
+                            print("Incorrect password! Please try again.")
+
+                    else:
+                        print("Username not found!")
+
+                else:
+                    print("Invalid password format!")
+            
+            else:
+                print("Invalid username format!")
+
+        else:
+            print("Invalid username format!")
+
+    def update_username(self):
+        """
+        This function updates the username of the user.
+        """
+
+        new_username = input("New Username: ")
+
+        # Check for proper username format
+        if len(new_username) != 0 and new_username.isalnum():
+
+            # Check that no existing account already has this username
+            result = User.query.filter_by(username=new_username).first()
+
+            # Once validated, update username
+            if result == None:
+                self.username = new_username
+                print("Username successfully updated!")
+
+            else:
+                print("Username already taken!")
+        else:
+            # factor in requirements about allowing spaces
+            if (" " in new_username and new_username[0] != " " and
+                new_username[-1] != " "):
+                temp_user = new_username.replace(" ", "")
+
+                # If no special characters other than space and format allowed
+                # Check that no existing account already has this username
+                if temp_user.isalnum():
+                    result = User.query.filter_by(username=new_username).first()
+
+                    # Once validated, update username
+                    if result == None:
+                        self.username = new_username
+                        print("Username successfully updated!")
+            else:
+                print("Invalid username format!")
+
+    def update_email(self):
+        """
+        This function updates the email of the user.
+        """
+
+        new_email = input("New Email: ")
+
+        # Check for proper email format
+        if len(new_email) != 0:
+            try:
+                valid_email = validate_email(new_email)
+                new_email = valid_email["email"]
+            except:
+                print("Invalid email format!")
+                return
+
+            # Check that no existing account already has this email address
+            result = User.query.filter_by(email=new_email).first()
+
+            # Once validated, update username
+            if result == None:
+                self.email = new_email
+                print("Email successfully updated!")
+
+            else:
+                print("Email already associated with existing account!")
+
+        else:
+            print("Invalid email format!")
+
+    def update_address(self):
+        """
+        This function updates the billing address of the user.
+        """
+        new_address = input("New Address: ")
+
+        # Check that an address has been entered
+        if len(new_address) != 0:
+            self.billing_address = new_address
+            print("Address successfully updated!")
+        else:
+            print("Invalid address format!")
+
+    def update_postal_code(self):
+        """
+        This function updates the postal code of the user.
+        """
+
+        new_postal_code = input("New Postal Code: ")
+
+        # Check for proper postal code format
+        if len(new_postal_code) != 0 and new_postal_code.isalnum():
+
+            # Once validated, update postal code
+            if (new_postal_code[0] in "ABCEGHJKLMNPRSTVXY" and
+                new_postal_code[1].isnumeric() and new_postal_code[3].isnumeric() and
+                new_postal_code[5].isnumeric() and new_postal_code[2].isalpha() and
+                new_postal_code[2].isalpha()):
+                # Checks for valid province/region code and valid format (ex. A1A1A1)
+                # TODO: Add more specific verification since not every
+                # character/combination is used
+
+                self.postal_code = new_postal_code
+                print("Postal Code successfully updated!")
+        else:
+            print("Invalid postal code format!")
