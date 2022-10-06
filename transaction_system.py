4e2f39bfc16cd1348a1b266b34b87b6e6ea85dad
Daniel <48493942+dadijo2002@users.noreply.github.com>
2022-10-05 20:44:55 -0400

    💚Starting to add SQL compatibility; still need to connect to other Classes
    Description
    Started ti implement SQL compatibility
    
    Author: # Daniel Dickson
    Fixes # 1
    
    Type of change
    Please delete options that are not relevant.
    
     New feature (non-breaking change which adds functionality)
    
     This change requires a documentation update
    Checklist:
    [x ] My code follows the style guidelines of this project (PEP 8)
    [ x] I have performed a self-review of my own code
    [ x] I have commented my code, particularly in hard-to-understand areas
    [ x] My changes generate no new warnings
    [ x] Any dependent changes have been merged and published in downstream modules

--- transaction_system.py
+++ transaction_system.py
@@ -5,50 +5,52 @@ our qBnb plan.
 Group 21 - CISC 327
 Author: Daniel Dickson
 Student Number: 20206675
-Date: September 23, 2022
+Date: October 7, 2022
 """
 
 # TODO: Reference a database (SQL) of all successful transactions
 # TODO: Generate an ID for successful transactions based on
 # this database
 
+# TODO: Link the class with databases from other classes for full functionality
+
 from datetime import date
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-user_db = {"name": "Felonius Gru",
-                  "balance": 1000000.00}
 
-transaction_db = {"unit cost": 1234.00,
-                  "unit name": "Minion Beach",
-                  "unit id": 16984,
-                  "seller": "Bob the Minion",
-                  "start date": 20220613,
-                  "end date": 20220701,
-                  "transaction id": 1,
-                  "availability": True}
-# this is an approximation of potential necessary values for a
-# transaction, these values are not final: more may be added
-# or some may be taken out in the final product
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# ^ need to decide what database we connect to?
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+t_db = SQLAlchemy(app)
 
 class Transaction:
 
-  def __init__(self, user, transaction_info, name, balance, unit_cost,
-               unit_name, unit_id, seller_name, start_date, end_date, transaction_id,
-               availability, validity):
-    self.user = user_db # user specific info
-    self.transaction_info = transaction_db
-    # transaction & property specific info
-    self.name = user_db["name"]
-    self.balance = user_db["balance"]
-    self.unit_cost = transaction_db["unit cost"]
-    self.unit_name = transaction_db["unit name"]
-    self.unit_id = transaction_db["unit id"]
-    self.seller_name = transaction_db["seller name"]
-    self.start_date = transaction_db["start date"]
-    self.end_date = transaction_db["end date"]
-    self.transaction_id = transaction_db["transaction id"]
-    self.availability = transaction_db["availability"]
+
+  t_id = t_db.Column(t_db.Integer, primary_key=True)
+  start_date = t_db.Column(t_db.Integer, nullable=False)
+  end_date = t_db.Column(t_db.Integer, nullable=False)
+
+  renter = t_db.relationship('renter', backref='user')
+  seller = t_db.relationship('seller', backref='user')
+  property = t_db.relationship('property', backref='listing')
+
+  def __init__(self, user, listing_info):
+    self.user = user # user specific info
+    self.username = user.username
+    self.balance = user.balance
+    self.unit_cost = listing_info.cost
+    self.unit_name = listing_info.name
+    self.unit_id = listing_info.id
+    self.seller_name = listing_info.seller
+    self.availability = listing_info.availability
     self.validity = False
     # default is False so that errors can be recognized later if any exist
+
+    # TODO: Figure out how to connect to other databases to get information,
+    # remove user.x and listing_info.x and replace with database info
 
   def check_balance(self):
     """
