4b76e1d2ab7f82010db4822113de68010991729c
Aniket <91762784+SnowKestrel@users.noreply.github.com>
2022-10-06 20:25:06 -0400

    Added further documentation
    More documentation and bug fixes. Also got rid of mySQL stuff as well

--- Review_System.py
+++ Review_System.py
@@ -1,6 +1,4 @@
-import mysql.connector
-import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 """
@@ -11,30 +9,40 @@ Author: Aniket Mukherjee
 Student Number: 20245057
 Date: October 3, 2022
 """
-# creating a mysql database to merge with SQLAlchemy code
-review_db = mysql.connector.connect(host="localhost",
-                                    user="root",
-                                    password="Splinter_Ce11",
-                                    database="review_database"
-
-                                    )
-review_cursor = review_db.cursor()
 
 app = Flask(__name__)
-# merging here
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/review_database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Review(db.Model):  # making each property a column in the database
+    """
+    This class creates a Review object for use within our QB&B project
+    Attributes:
+        identification (Integer): This serves as the review id
+        user_id (Integer): This serves as the user id
+        listing_id (Integer): Serves as the id for the listing
+        review_content(Str): Serves as the content of the review itself
+        date(str): The date when the review was made
+
+    """
     identification = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, primary_key=True)
     listing_id = db.Column(db.Integer, primary_key=True)
-    review_content = db.Column(db.String(500), Unique=True, nullable=False)
-    date = db.Column(db.String(50), Unique=False, Nullable=False)
+    review_content = db.Column(db.String(500), unique=True, nullable=False)
+    date = db.Column(db.String(50), unique=False, nullable=False)
 
-    def __init__(self, identification, user_id, listing_id, review_content, date):
+    def __init__(self, identification, user_id, listing_id,
+                 review_content, date):
+        """
+        :param identification:
+        :param user_id:
+        :param listing_id:
+        :param review_content:
+        :param date:
+        """
+        # Initializes a review object with it's attributes
 
         self.identification = identification
         self.user_id = user_id
@@ -43,7 +51,11 @@ class Review(db.Model):  # making each property a column in the database
         self.date = date
 
     def validate(self):
-        # checking to see if the review is valid, and none of the fields can't be accounted for
+        """
+        :return Boolean:
+        """
+        # checking to see if the review is valid,
+        # and none of the fields can't be accounted for
         if self.identification == "" or self.user_id == "" or \
                 self.listing_id == "" or self.review_content == "" or \
                 self.date == "":
