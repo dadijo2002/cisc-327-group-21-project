1471c1aab6ad3f3c1af51d7dbfcdeb4477d2a015
Yash Patel <yashnilay@gmail.com>
2022-10-11 18:11:55 -0300

    Updated Listings system, and Added Listings backend

--- listings_backend.py
+++ listings_backend.py
@@ -4,14 +4,17 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 """
-R4-1: The title of the product has to be alphanumeric-only, and space allowed only if it is not as prefix and suffix.
-R4-2: The title of the product is no longer than 80 characters.
-R4-3: The description of the product can be arbitrary characters, with a minimum length of 20 characters and a maximum of 2000 characters.
+R4-1: The title of the product has to be alphanumeric-only, and space allowed
+ only if it is not as prefix and suffix.
+R4-2: The title of the product no longer than 80 characters.
+R4-3: The description of the product can be arbitrary characters,
+ with a minimum length of 20 characters and a maximum of 2000 characters.
 R4-4: Description has to be longer than the product's title.
 """
 
 """
-This is an incomplete sample implementation of some of the backend features for the listings class
+This is an incomplete sample implementation of some of the backend features
+ for the listings class
 Group 21 - CISC 327
 Author: Aniket Mukherjee
 Student Number: 20245057
@@ -32,18 +35,58 @@ class Listings(db.Model):  # SQLified the old Listings class a little bit
     amenities = db.Column(db.String(120), unique=True, nullable=False)
     description = db.Column(db.String(2000), unique=True, nullable=False)
     availability = db.Column(db.String(10), unique=True, nullable=False)
+    last_modified_date = db.Column(db.String(50), unique=True, nullable=False)
+    owner_email = db.Column(db.String(120), unique=True, nullable=False)
 
     def title_check(self):
         # This serves to satisfy requirements R4-1 and R4-2
-        if self.title.isalnum() and (self.title.find(" ") == 0 or self.title.find(" ") == self.username.length) and len(
+        if self.title.isalnum() and (self.title.find(" ") == 0 or
+                                     self.title.find(" ") ==
+                                     self.username.length) and len(
                 self.title) <= 80:
             return True
         else:
             return False
 
     def description_check(self):
-        # this serves to satisfy requirements R4-3 and R4-4
-        if len(self.description) < 20 or len(self.description) > 2000 or len(self.description) < len(self.title):
+        # this serves to satisfy requirements R4-3, R4-4
+        if len(self.description) < 20 or len(self.description) > 2000 or \
+                len(self.description) < len(self.title):
+            return False
+        else:
+            return True
+
+    # R4-5: Price has to be of range [10, 10000]
+    def price_validation(self):
+        # checking to see if the price is valid
+        if self.price < 10 or self.price > 10000:
+            return False
+        else:
+            return True
+
+    # R4-6: Last_modified_date must be after 2021-01-02 and before 2025-01-02
+
+    def date_validation(self):
+        # checking to see if the date is valid
+        if self.last_modified_date < "2021-01-02" or \
+                self.last_modified_date > "2025-01-02":
+            return False
+        else:
+            return True
+
+    # R4-7: owner_email cannot be empty. The owner of the corresponding
+    # product must exist in the database
+    def owner_email_validation(self):
+        # checking to see if the owner email is valid
+        if self.owner_email == "" or self.owner_email not in db.Model:
+            return False
+        else:
+            return True
+
+    # R4-8: A user cannot create products that have the same title
+    def title_validation(self):
+        # checking to see if the title is valid
+        if self.title in db.Model:
             return False
         else:
             return True
--- listings_system.py
+++ listings_system.py
@@ -1,3 +1,10 @@
+import mysql.connector
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
 """
 This is an incomplete listings 
 system for the qBnB plan.
@@ -9,31 +16,23 @@ Date: September 25, 2022
 # This block of code represents a simulation of a listing entry in a database
 # I have read through the airbnb website and made a checklist of what
 # could be included for the listing entries;
-listing_db = {"Host": "Bob Dylan",
-              "Title": "groovy Cottage",
-              "Location": "Sault Ste. Marie, Ontario",
-              "Price Per Night": 450,
-              "Amount of Guests": 8,
-              "Amenities": ["Wi-fi", "Kitchen", "TV", "Washer", "Dryer"],
-              "Description": "A groovy cottage in Guelph, Ontario",
-              "Availability": "1997-10-9 to 2024-12-12",  # Year | month | day!
-              }
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-class listing:
-    # Main listing class
-    def __init__(self, host, title, location, price_per_night,
-                 guests, amenities, description, availability):
-        # Initialize the database jazz
-        self.host = listing_db["Host"]
-        self.title = listing_db["Title"]
-        self.location = listing_db["Location"]
-        self.price_per_night = listing_db["Price Per Night"]
-        self.guests = listing_db["Amount of Guests"]
-        self.amenities = listing_db["Amenities"]
-        self.description = listing_db["Description"]
-        self.availability = listing_db["Availability"]
-        self.valid = False  # Default value is false for future error spotting
+class Listings(db.Model):  # SQLified the old Listings class a little bit
+    host = db.Column(db.String(80), Unique=True, nullable=False)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    location = db.Column(db.String(120), unique=True, nullable=False)
+    price_per_night = db.Column(db.Integer, primary_key=True)
+    amenities = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(2000), unique=True, nullable=False)
+    availability = db.Column(db.String(10), unique=True, nullable=False)
+    last_modified_date = db.Column(db.String(50), unique=True, nullable=False)
+    owner_email = db.Column(db.String(120), unique=True, nullable=False)
 
     def validate(self):
         """
@@ -67,15 +66,62 @@ class listing:
         It will check if the inputted listing is valid,
         by calling the validate function,and if it
         returns true, it will add the listing to the listing db.
-
-        THIS is not complete yet, todo: create a database using
-        sql or mariadb or something.
         """
+
         if self.validate() is True:
-            listing_db.append(self.host, self.title, self.location,
-                              self.price_per_night,
-                              self.guests, self.amenities,
-                              self.description, self.availability)
+            db.Model.append(self.host, self.title, self.location,
+                            self.price_per_night,
+                            self.guests, self.amenities,
+                            self.description, self.availability)
             return True
         else:
             return False
+
+    # R5-1: User can change all attributes of a listing, except owner_id
+    # and last_modified_date
+    def change_listing(self):
+        """
+        This Function will change the listing information
+        of the listing that the user is looking at.
+        It will ask the user if they want to change the
+        host, title, location, price per night, guests,
+        amenities, description, or availability of the listing.
+        It will then ask the user to input the new information
+        for the attribute they want to change.
+        """
+        print("Which attribute of the listing do you want to change?")
+        print("1: Host")
+        print("2: Title")
+        print("3: Location")
+        print("4: Price Per Night")
+        print("5: Guests")
+        print("6: Amenities")
+        print("7: Description")
+        print("8: Availability")
+        print("9: Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            self.host = input("Enter the new host: ")
+        elif choice == "2":
+            self.title = input("Enter the new title: ")
+        elif choice == "3":
+            self.location = input("Enter the new location: ")
+        elif choice == "4":
+            self.price_per_night += input(
+                "User can only increase this value, Enter the amount you would \
+                 like to increase the price by: ")
+        elif choice == "5":
+            self.guests = input("Enter the new amount of guests: ")
+        elif choice == "6":
+            self.amenities = input("Enter the new amenities: ")
+        elif choice == "7":
+            self.description = input("Enter the new description: ")
+        elif choice == "8":
+            self.availability = input("Enter the new availability: ")
+        elif choice == "9":
+            return
+        else:
+            print("Invalid choice.")
+
+        # Here is where last_modified_date would be updated
+        self.last_modified_date = datetime.datetime.now()
