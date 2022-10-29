600604d8253c95f14c42f669f82a999ee965896d
Aniket <91762784+SnowKestrel@users.noreply.github.com>
2022-10-28 20:16:05 -0400

    added some new driver code to the start

--- qbnb/__main__.py
+++ qbnb/__main__.py
@@ -2,10 +2,12 @@ from qbnb import app
 from qbnb.models import *
 from qbnb.controllers import *
 
+
 """
 This file runs the server at a given port
 """
-
+sqlalchemy = SQLAlchemy()
+sqlalchemy.init_app(app)
 FLASK_PORT = 8081
 
 if __name__ == "__main__":
