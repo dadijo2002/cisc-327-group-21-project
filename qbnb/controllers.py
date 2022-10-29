9083c85189dcf3d86f9b38116a4aefc5d0a85595
Daniel <48493942+dadijo2002@users.noreply.github.com>
2022-10-28 21:50:21 -0400

    Minor fix in controllers.py

--- qbnb/controllers.py
+++ qbnb/controllers.py
@@ -61,7 +61,7 @@ def login_post():
         """
         # success! go back to the home page
         # code 303 is to force a 'GET' request
-        return redirect('index.html', code=303)
+        return redirect('index.html')
     else:
         return render_template('login.html', message='login failed')
 
