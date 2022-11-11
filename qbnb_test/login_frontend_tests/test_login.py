779c9f0de8794699b7ecec8146583d2cb447bdb9
Aniket <91762784+SnowKestrel@users.noreply.github.com>
2022-11-11 16:01:16 -0500

    Added more documentation and testcases

--- qbnb_test/login_frontend_tests/test_login.py
+++ qbnb_test/login_frontend_tests/test_login.py
@@ -46,3 +46,36 @@ class test_login_page(BaseCase):
         self.open(base_url)
         self.assert_element("#error_header")
         self.assert_text("Error! You didn't input an email address!", "error-header")
+
+    def test_login_email_valid(self, *_):
+        """
+        Tests if invalid email input sends error
+        Test method: Black Box input partitioning
+        """
+        self.open(base_url + '/login')
+        self.type("#email", "test1test.com")
+        self.type("#password", "_TaskF141")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Login failed", "#message")
+
+    def test_login_boundaries(self, *_):
+        """
+        Tests if the password or email inputs outside boundaries
+        cause an error
+        Test method: Black Box Input Boundary Testing
+        """
+
+        self.open(base_url + '/login')
+        self.type("#email", "test1@test.com")
+        self.type("#password", "_Aa123333333333333333333333333333333333\
+                          333333333333333333333333333333333333333333")
+        self.assert_element("#message")
+        self.assert_text("Login failed", "#message")
+
+        self.type("#email", "test1@test.com")
+        self.type("#password", "_Aa12")
+        self.click('input[type="submit"]')
+        # test error message is thrown
+        self.assert_element("#message")
+        self.assert_text("Login failed.", "#message")
