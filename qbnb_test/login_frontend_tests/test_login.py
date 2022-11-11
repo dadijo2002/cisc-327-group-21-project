fb166f52fc5e22d344f5814d9c0ca8abdb970c71
Aniket <91762784+SnowKestrel@users.noreply.github.com>
2022-11-11 14:40:57 -0500

    Added something I forgot
    The black box testing classifications

--- qbnb_test/login_frontend_tests/test_login.py
+++ qbnb_test/login_frontend_tests/test_login.py
@@ -7,6 +7,11 @@ from qbnb.models import *
 class test_login_page(BaseCase):
 
     def test_login_success(self, *_):
+        """
+        Tests if the login page can log a user in
+        successfully and take them to the user homepage
+        Test method: Black Box exhaustive output coverage (1 of 3)
+        """
         self.open(base_url + '/login')
         self.type("#email", "test0@test.com")
         self.type("#password", "123456")
@@ -17,6 +22,10 @@ class test_login_page(BaseCase):
         self.assert_text("Welcome u0 !", "welcome-header")
 
     def test_login_pw(self, *_):  # try to make a test with no password
+        """
+        Tests if the password field is unfilled
+        Test method: Black Box exhaustive output coverage(2 of 3)
+        """
         self.open(base_url + '/login')
         self.type("#email", "test1@test.com")
         self.type("#password", " ")
@@ -26,6 +35,10 @@ class test_login_page(BaseCase):
         self.assert_text("Error! You didn't input a password", "error-header")
 
     def test_login_email(self, *_):
+        """
+        Tests if the email field is unfilled
+        Test method: Black Box exhaustive output coverage(3 of 3)
+        """
         self.open(base_url + '/login')
         self.type("#email", " ")
         self.type("#password", "hamburgers")
