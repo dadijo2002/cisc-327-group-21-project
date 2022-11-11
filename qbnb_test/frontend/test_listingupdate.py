31adb3fe40b9c492fde49f9498efe28d6122cd10
Daniel <48493942+dadijo2002@users.noreply.github.com>
2022-11-10 23:05:08 -0500

    💚Added tests for index and update listing
    # Description
    
    Please include a summary of the change and which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change.
    
    Author: # Daniel Dickson
    Fixes # 1
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [X] New feature (non-breaking change which adds functionality) [test code]
    
    # How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide instructions so we can reproduce. Please also list any relevant details for your test configuration
    If there were not any tests completed for this, please make sure the comments on your code have insightful information for future reference.
    - [ ] Run on someone else's system bc mine is still not working
    
    # Checklist:
    
    - [X] My code follows the style guidelines of this project (PEP 8)
    - [X] I have performed a self-review of my own code
    - [X] I have commented my code, particularly in hard-to-understand areas
    - [X] My changes generate no new warnings
    - [X] Any dependent changes have been merged and published in downstream modules

---
+++ qbnb_test/frontend/test_listingupdate.py
@@ -0,0 +1,59 @@
+from seleniumbase import BaseCase
+
+from qbnb_test.conftest import base_url
+from unittest.mock import patch
+from qbnb.models import User
+
+# create test user and example listing
+register("test", "test@grouptwentyone.ca", "FUNpswd123?!")
+create_product("Joseph Mother", "New Listing", "1234 Sesame Street",
+                "6", "none :(", "This property is the best thing since sliced bread!", 
+                "2022-06-09", "2023-04-20")
+
+"""
+This file defines all integration tests for the frontend homepage.
+"""
+
+class FrontEndHomePageTest(BaseCase):
+
+    def setup(self):
+        """
+        Log in to start testing
+        """
+        # simulate login
+        self.open(base_url + "/login")
+        self.type("#email", "test@grouptwentyone.ca")
+        self.type("#password", "FUNpswd123?!")
+        self.click("input[type=\"submit\"]")
+        self.click_link("update")
+
+    def test_cancelupdate(self):
+        """
+        Test when user cancels update
+        """
+        self.click_link("Cancel")
+        self.assert_equal(self.get_current_url(), base_url + "/")
+
+    def test_successfulupdate(self):
+        """
+        Test that products can be updated successfully
+        """
+        # test all fields can be modified
+        self.type("#host", "Joe Mama")
+        self.type("#title", "Newer Product")
+        self.type("#location", "1273 Rockefeller Street")
+        self.type("#guests", "5")
+        self.type("#amenities", "Sliced Bread, Baked Beans, and a nice view")
+        self.type("#description", "This property is better than sliced bread!")
+        self.type("#ppn", "100")
+        self.type("#startavail", "2023-03-19")
+        self.type("#endavail", "2024-03-19")
+        # make sure on the correct page
+        self.assert_equal(self.get_current_url(), base_url + "/update-product/")
+        # submit changes
+        self.click("input[type=\"submit\"]")
+
+        # If product update successful, return to profile page
+        self.assert_equal(self.get_current_url(), base_url + "/")
+        # Product list on homepage should now be updated
+        self.assert_text("Newer Product")
