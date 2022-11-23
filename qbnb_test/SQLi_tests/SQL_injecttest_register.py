97fe8420a414046d997ae8734f849694bda020b3
Makayla <makayla_mcmullin@LAPTOP-HT55OM6A.localdomain>
2022-11-22 21:08:25 -0500

    can now run functions separately

--- qbnb_test/SQLi_tests/SQL_injecttest_register.py
+++ qbnb_test/SQLi_tests/SQL_injecttest_register.py
@@ -1,16 +1,24 @@
-from qbnb.models import User 
+from qbnb.models import * 
 
-payloads= open('Generic_SQLI.txt', 'r' )
-lines= payloads.readlines()
+
 
 def test_username_parameter():
+    payloads= open('Generic_SQLI.txt', 'r' )
+    lines= payloads.readlines()
+    payloads.close()
+    
     for line in lines:
         try:
             register("u0","test0@yahoo.com", "_Aa1234", "_Aa1234" )
         except (TypeError,ValueError):
             print("An SQL Injection attack was stopped.")
+    
 
 def test_email_parameter():
+    payloads= open('Generic_SQLI.txt', 'r' )
+    lines= payloads.readlines()
+    payloads.close()
+
     for line in lines:
         try:
             register("u1","test1yahoo.com", "_Aa1234", "_Aa1234" )
@@ -19,6 +27,10 @@ def test_email_parameter():
 
 
 def test_password_parameter():
+    payloads= open('Generic_SQLI.txt', 'r' )
+    lines= payloads.readlines()
+    payloads.close()
+
     for line in lines:
         try:
             register("u2","test2@yahoo.com", "_Aa1234", "_Aa1234" )
@@ -26,10 +38,13 @@ def test_password_parameter():
             print("An SQL Injection attack was stopped.")
 
 def test_password2_parameter():
+    payloads= open('Generic_SQLI.txt', 'r' )
+    lines= payloads.readlines()
+    payloads.close()
+
     for line in lines:
         try:
             register("u3","test3@yahoo.com", "_Aa1234", "_Aa1234" )
         except (TypeError,ValueError):
             print("An SQL Injection attack was stopped.")
 
-payloads.close()
